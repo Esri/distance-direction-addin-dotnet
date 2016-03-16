c36d7c1ab0a7bba736497bf3196bfa08b96dc662
Dan Hagens <danhagens@gmail.com>
2016-03-16 15:22:23 -0400

    Removed property variable

--- source/GeodesyAndRange/ArcMapAddinGeodesyAndRange/ArcMapAddinGeodesyAndRange/Models/Graphic.cs
+++ source/GeodesyAndRange/ArcMapAddinGeodesyAndRange/ArcMapAddinGeodesyAndRange/Models/Graphic.cs
@@ -12,32 +12,29 @@ namespace ArcMapAddinGeodesyAndRange.Models
     {
         public Graphic(GraphicTypes _graphicType, string _uniqueid, IGeometry _geometry, bool _isTemp = false)
         {
-            graphicType = _graphicType;
-            uniqueId = _uniqueid;
-            geometry = _geometry;
-            isTemp = _isTemp;
+            GraphicType = _graphicType;
+            UniqueId = _uniqueid;
+            Geometry = _geometry;
+            IsTemp = _isTemp;
         }
         
         // properties   
-        public GraphicTypes graphicType = GraphicTypes.Line;
+
         /// <summary>
         /// Property for the graphic type
         /// </summary>
         public GraphicTypes GraphicType {get;set;}
 
-        public string uniqueId = "";
         /// <summary>
         /// Property for the unique id of the graphic (guid)
         /// </summary>
         public string UniqueId {get;set;}
 
-        public IGeometry geometry = null;
         /// <summary>
         /// Property for the geometry of the graphic
         /// </summary>
         public IGeometry Geometry {get;set;}
 
-        public bool isTemp = false;
         /// <summary>
         /// Property to determine if graphic is temporary or not
         /// </summary>
