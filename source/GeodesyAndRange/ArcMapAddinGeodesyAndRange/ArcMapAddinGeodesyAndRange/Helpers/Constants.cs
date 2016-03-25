642550f00bdc0d33def3335b4214630539900eee
Joseph McCausland <jmccausland@esri.com>
2016-03-25 10:55:06 -0400

    Update formatted points when display type is changed

--- source/GeodesyAndRange/ArcMapAddinGeodesyAndRange/ArcMapAddinGeodesyAndRange/Helpers/Constants.cs
+++ source/GeodesyAndRange/ArcMapAddinGeodesyAndRange/ArcMapAddinGeodesyAndRange/Helpers/Constants.cs
@@ -27,5 +27,6 @@ namespace ArcMapAddinGeodesyAndRange
         public const string NEW_MAP_POINT = "NEW_MAP_POINT";
         public const string MOUSE_MOVE_POINT = "MOUSE_MOVE_POINT";
         public const string TAB_ITEM_SELECTED = "TAB_ITEM_SELECTED";
+        public const string DISPLAY_COORDINATE_TYPE_CHANGED = "DISPLAY_COORDINATE_TYPE_CHANGED";
     }
 }
