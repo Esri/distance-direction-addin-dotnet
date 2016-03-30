daaf7fffcb8d7c0201d17eaac08af65d035d3a1b
Joseph McCausland <jmccausland@esri.com>
2016-03-30 11:10:23 -0400

    Initial commit for ProAddin

---
+++ source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/DistanceAndDirectionModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using System.Threading.Tasks;
+
+namespace ProAppDistanceAndDirectionModule
+{
+    internal class DistanceAndDirectionModule : Module
+    {
+        private static DistanceAndDirectionModule _this = null;
+
+        /// <summary>
+        /// Retrieve the singleton instance to this module here
+        /// </summary>
+        public static DistanceAndDirectionModule Current
+        {
+            get
+            {
+                return _this ?? (_this = (DistanceAndDirectionModule)FrameworkApplication.FindModule("ProAppDistanceAndDirectionModule_Module"));
+            }
+        }
+
+        #region Overrides
+        /// <summary>
+        /// Called by Framework when ArcGIS Pro is closing
+        /// </summary>
+        /// <returns>False to prevent Pro from closing, otherwise True</returns>
+        protected override bool CanUnload()
+        {
+            //TODO - add your business logic
+            //return false to ~cancel~ Application close
+            return true;
+        }
+
+        #endregion Overrides
+
+    }
+}
---
+++ source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/PropertiesButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace ProAppDistanceAndDirectionModule
+{
+    internal class PropertiesButton : Button
+    {
+        protected override void OnClick()
+        {
+        }
+    }
+}
