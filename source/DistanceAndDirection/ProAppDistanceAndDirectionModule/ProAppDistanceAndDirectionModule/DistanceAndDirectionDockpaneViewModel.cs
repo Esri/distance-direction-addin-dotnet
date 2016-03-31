f4da9f9121a19abeaf41a40334c42785805d3615
Joseph McCausland <jmccausland@esri.com>
2016-03-31 09:02:39 -0400

    Move mainviewmodel to proper pro template viewmodel

--- source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/DistanceAndDirectionDockpaneViewModel.cs
+++ source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/DistanceAndDirectionDockpaneViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
+using DistanceAndDirectionLibrary.Views;
 
 namespace ProAppDistanceAndDirectionModule
 {
@@ -12,7 +13,21 @@ namespace ProAppDistanceAndDirectionModule
     {
         private const string _dockPaneID = "ProAppDistanceAndDirectionModule_DistanceAndDirectionDockpane";
 
-        protected DistanceAndDirectionDockpaneViewModel() { }
+        protected DistanceAndDirectionDockpaneViewModel() 
+        {
+            // set some views and datacontext
+            LinesView = new GRLinesView();
+            //LinesView.DataContext = new LinesViewModel();
+
+            CircleView = new GRCircleView();
+            //CircleView.DataContext = new CircleViewModel();
+
+            EllipseView = new GREllipseView();
+            //EllipseView.DataContext = new EllipseViewModel();
+
+            RangeView = new GRRangeView();
+            //RangeView.DataContext = new RangeViewModel();
+        }
 
         /// <summary>
         /// Show the DockPane.
@@ -29,15 +44,44 @@ namespace ProAppDistanceAndDirectionModule
         /// <summary>
         /// Text shown near the top of the DockPane.
         /// </summary>
-        private string _heading = "My DockPane";
-        public string Heading
+        //private string _heading = "My DockPane";
+        //public string Heading
+        //{
+        //    get { return _heading; }
+        //    set
+        //    {
+        //        SetProperty(ref _heading, value, () => Heading);
+        //    }
+        //}
+
+        #region Properties
+
+        object selectedTab = null;
+        public object SelectedTab
         {
-            get { return _heading; }
+            get { return selectedTab; }
             set
             {
-                SetProperty(ref _heading, value, () => Heading);
+                if (selectedTab == value)
+                    return;
+
+                selectedTab = value;
+                //var tabItem = selectedTab as TabItem;
+                //Mediator.NotifyColleagues(Constants.TAB_ITEM_SELECTED, ((tabItem.Content as UserControl).Content as UserControl).DataContext);
             }
         }
+
+        #region Views
+
+        public GRLinesView LinesView { get; set; }
+        public GRCircleView CircleView { get; set; }
+        public GREllipseView EllipseView { get; set; }
+        public GRRangeView RangeView { get; set; }
+
+        #endregion
+
+        #endregion
+
     }
 
     /// <summary>
