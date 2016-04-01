ef6d701cf5fe3609f740144d7d1281977e5a1817
Joseph McCausland <jmccausland@esri.com>
2016-04-01 10:28:21 -0400

    add sketch tool, add properties view model for Pro

---
+++ source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/SketchTool.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Mapping;
+using DistanceAndDirectionLibrary.Helpers;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+
+namespace ProAppDistanceAndDirectionModule
+{
+    class SketchTool : MapTool
+    {
+        public SketchTool()
+        {
+            // we may use the sketch tool if it supports geodesic feedback
+            // but it doesn't look like it does
+            // need to see what geodesic support is available
+            IsSketchTool = true;
+            SketchType = SketchGeometryType.Point;
+            SketchOutputMode = SketchOutputMode.Map;
+            Mediator.Register("SET_SKETCH_TOOL_TYPE", (sgType) => SketchType = (SketchGeometryType)sgType);
+        }
+
+        protected override Task<bool> OnSketchCompleteAsync(Geometry geometry)
+        {
+            QueuedTask.Run(() =>
+                {
+                    Mediator.NotifyColleagues("SKETCH_COMPLETE", geometry);
+                });
+            return base.OnSketchCompleteAsync(geometry);
+        }
+
+        protected override void OnToolMouseDown(MapViewMouseButtonEventArgs e)
+        {
+            QueuedTask.Run(() =>
+            {
+                var mp = MapView.Active.ClientToMap(e.ClientPoint);
+                Mediator.NotifyColleagues(DistanceAndDirectionLibrary.Constants.NEW_MAP_POINT, mp);
+            });
+            base.OnToolMouseDown(e);
+        }
+
+        protected override void OnToolMouseMove(MapViewMouseEventArgs e)
+        {
+            QueuedTask.Run(() =>
+            {
+                var mp = MapView.Active.ClientToMap(e.ClientPoint);
+                Mediator.NotifyColleagues(DistanceAndDirectionLibrary.Constants.MOUSE_MOVE_POINT, mp);
+            });
+            base.OnToolMouseMove(e);
+        }
+
+        protected override void OnToolDoubleClick(MapViewMouseButtonEventArgs e)
+        {
+            QueuedTask.Run(() =>
+            {
+                var mp = MapView.Active.ClientToMap(e.ClientPoint);
+                Mediator.NotifyColleagues(DistanceAndDirectionLibrary.Constants.MOUSE_DOUBLE_CLICK, mp);
+            });
+            base.OnToolDoubleClick(e);
+        }
+    }
+}
--- source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/ViewModels/ProTabBaseViewModel.cs
+++ source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/ViewModels/ProTabBaseViewModel.cs
@@ -1,20 +1,60 @@
-﻿using System;
+﻿// Copyright 2016 Esri 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DistanceAndDirectionLibrary.ViewModels;
 using ArcGIS.Core.Geometry;
 using ArcGIS.Desktop.Mapping;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Core.CIM;
+using ArcGIS.Desktop.Framework;
+using DistanceAndDirectionLibrary.Views;
+using DistanceAndDirectionLibrary.ViewModels;
 
 namespace ProAppDistanceAndDirectionModule.ViewModels
 {
     public class ProTabBaseViewModel : BaseViewModel
     {
         public ProTabBaseViewModel()
-        { }
+        {
+            // commands
+            EditPropertiesDialogCommand = new RelayCommand(() => OnEditPropertiesDialog());
+
+        }
+
+        #region Commands
+
+        public RelayCommand EditPropertiesDialogCommand { get; set; }
+
+        /// <summary>
+        /// Handler for opening the edit properties dialog
+        /// </summary>
+        /// <param name="obj"></param>
+        private void OnEditPropertiesDialog()
+        {
+            var dlg = new EditPropertiesView();
+            dlg.DataContext = new ProEditPropertiesViewModel();
+
+            dlg.ShowDialog();
+        }
+        
+        #endregion
+
+
 
         internal async void AddGraphicToMap(Geometry geom, bool IsTempGraphic = false, double size = 1.0)
         {
