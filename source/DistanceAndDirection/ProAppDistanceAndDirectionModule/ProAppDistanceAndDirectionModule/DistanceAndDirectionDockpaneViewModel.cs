111eefb1c5ebf40fea30f6b0b6b8d4897ca5e80b
Joseph McCausland <jmccausland@esri.com>
2016-04-07 15:47:22 -0400

    check content for null

--- source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/DistanceAndDirectionDockpaneViewModel.cs
+++ source/DistanceAndDirection/ProAppDistanceAndDirectionModule/ProAppDistanceAndDirectionModule/DistanceAndDirectionDockpaneViewModel.cs
@@ -71,7 +71,8 @@ namespace ProAppDistanceAndDirectionModule
 
                 selectedTab = value;
                 var tabItem = selectedTab as TabItem;
-                Mediator.NotifyColleagues(Constants.TAB_ITEM_SELECTED, ((tabItem.Content as UserControl).Content as UserControl).DataContext);
+                if ((tabItem.Content as UserControl).Content != null)
+                    Mediator.NotifyColleagues(Constants.TAB_ITEM_SELECTED, ((tabItem.Content as UserControl).Content as UserControl).DataContext);
             }
         }
 
