9a13e13d210e875383ea7894cd9855b14d4eef66
Kevin Gonzago <kgonzago@users.noreply.github.com>
2018-04-26 10:39:55 -0400

    Revert "Addresses issue 312"

--- source/addins/DistanceAndDirectionLibrary/Properties/Resources.Designer.cs
+++ source/addins/DistanceAndDirectionLibrary/Properties/Resources.Designer.cs
@@ -835,15 +835,6 @@ namespace DistanceAndDirectionLibrary.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Number of rings must match the number of intervals supplied.
-        /// </summary>
-        public static string RingsIntervalsMismatchError {
-            get {
-                return ResourceManager.GetString("RingsIntervalsMismatchError", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Edit Properties.
         /// </summary>
         public static string TitleEditProperties {
@@ -867,15 +858,6 @@ namespace DistanceAndDirectionLibrary.Properties {
         public static string TooltipMapPointTool {
             get {
                 return ResourceManager.GetString("TooltipMapPointTool", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Ring interval must be more than 0 in length.
-        /// </summary>
-        public static string ZeroLengthIntervalError {
-            get {
-                return ResourceManager.GetString("ZeroLengthIntervalError", resourceCulture);
             }
         }
     }
